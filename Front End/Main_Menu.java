dff2c55101fac657313bde050bb9211d124b3a69
Faaris Ahmed <faaris415@gmail.com>
2022-01-26 20:12:55 +0000

    Added the functionality of the button so that when a button is pressed then the next page is loaded and shown

--- Front End/Main_Menu.java
+++ Front End/Main_Menu.java
@@ -1,6 +1,12 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class Main_Menu {
 
@@ -11,13 +17,17 @@ public class Main_Menu {
     private Button Staff_menu;
 
     @FXML
-    void Customer_Menu(ActionEvent event) {
+    void Staff_Menu(ActionEvent event) {
 
     }
 
     @FXML
-    void Staff_Menu(ActionEvent event) {
-
+    void Customer_Menu() throws IOException {
+      Stage primaryStage = new Stage();
+      Parent root = FXMLLoader.load(getClass().getResource("/Customer_menu.fxml"));
+      Scene scene = new Scene(root, 1200, 900);
+      primaryStage.setScene(scene);
+      primaryStage.show();
     }
 
 }
