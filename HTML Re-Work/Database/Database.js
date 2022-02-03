46c3d49b6a7c886e1305968b0c4eaeb3ab96b698
ManuKaur145 <zhac080@live.rhul.ac.uk>
2022-02-03 22:51:49 +0000

    Added a query to drop the database if it already exists

--- HTML Re-Work/Database/Database.js
+++ HTML Re-Work/Database/Database.js
@@ -3,14 +3,24 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Team"
+  password: "Manpreet145"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  con.query("DROP DATABASE IF EXISTS mydb", function (err, result){
+    if(err) throw err;
+    console.log("Database dropped");
+  });
   con.query("CREATE DATABASE mydb", function (err, result) {
     if (err) throw err;
     console.log("Database created");
   });
 });
+
+
+
+
+
+
