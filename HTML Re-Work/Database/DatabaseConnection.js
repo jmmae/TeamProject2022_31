6853ea3d1a9788f79cef2d04487e756f061e6eec
ZJAC101 <ZJAC101@live.rhul.ac.uk>
2022-02-24 16:41:30 +0000

    added .post '/order/unconfirmed' which recives an array of ordered dishes from the client and a .get '/order/getDishes' which returns an array of ordered dishes and their table

--- HTML Re-Work/Database/DatabaseConnection.js
+++ HTML Re-Work/Database/DatabaseConnection.js
@@ -14,14 +14,18 @@ const express = require('express')
 const app = express()
 const port = 3000 //the port this server is watching
 var cors = require('cors')
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+const { TIME, TIME2 } = require('mysql/lib/protocol/constants/types');
 var jsonParser = bodyParser.json()//jsonParser is used to be able to read the incoming data when encoded in json format
 var urlencodedParser = bodyParser.urlencoded({ extended: false })//urlencodedParser is used to be able to read the incoming data when encoded in url format
 
 var waiterRequests = new Array();
+var dishRequests = new Array();
+
+var nextOrderID = 0
 
 app.use(cors());
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({ limit: '1mb' }));
 
 //get is used when no data needs to be sent
 //'/' is the path that is beeing checked
@@ -52,31 +56,69 @@ app.get('/menu', function (req, res) {
 
 app.get('/menu/available', function (req, res) {
   console.log("Available menu request recieved");
-  selectquery("SELECT * FROM menu WHERE Available IS TRUE ;", res)
+  selectquery("SELECT * FROM menu WHERE available IS TRUE ;", res)
 })
 
 
 app.post('/order/requestWaiter/add', urlencodedParser, function (req, res) {
   console.log("Waiter add request recieved");
-  if (waiterRequests.includes(req.body.table) == false){
+  if (waiterRequests.includes(req.body.table) == false) {
     waiterRequests.push(req.body.table);
-    
-  } 
+
+  }
   res.send("Waiter add request recived")
 })
 
-// app.post('/order/requestWaiter/remove', urlencodedParser, function (req, res) {
-//   if (!waiterRequests.includes(req.body.table)){
-//     //waiterRequests.splice(waiterRequests.indexOf(req.body.table,1))
-    
-//   } else {
-    
-//     waiterRequests.splice(waiterRequests.indexOf(req.body.table),1)
-//   }
-//   console.log("Waiter remove request recieved");
-//   //console.log(waiterRequests);
-//   res.send("Waiter remove request recived")
-// })
+app.post('/order/unconfirmed', urlencodedParser, function (req, res) {
+  console.log("Waiter add request recieved");
+  dishRequests.push(req.body.table);
+  for (var i = 0; req.body.dishes[i] < req.body.dishes.length; i++) {
+    dishRequests.push(req.body.dishes[i]);
+  }
+  //query(/*delete all entries related to req.body.*/)
+  //  query("INSERT INTO order (/* fields */) VALUES ("+nextOrderID+","+TIME+ "," +req.body.table+"False)")
+  //  for (var i = 0; req.body.dishes[i] < req.body.dishes.length; i++) {
+  //    var entries = req.body.dishes[i].split("+")
+  //    query("INSERT INTO OrderedDish VALUES (" + entries[0] + "," + entries[1] + ",False)")
+  //  }
+
+  //  if (waiterRequests.includes(req.body.table) == false) {
+  //    waiterRequests.push(req.body.table);
+
+  //  }
+  //  res.send("Waiter add request recived")
+})
+
+app.get('/order/getDishes', urlencodedParser, function (req, res) {
+  console.log("Waiter add request recieved");
+  res.push(dishRequests);
+
+  //query(/*delete all entries related to req.body.*/)
+  //  query("INSERT INTO order (/* fields */) VALUES ("+nextOrderID+","+TIME+ "," +req.body.table+"False)")
+  //  for (var i = 0; req.body.dishes[i] < req.body.dishes.length; i++) {
+  //    var entries = req.body.dishes[i].split("+")
+  //    query("INSERT INTO OrderedDish VALUES (" + entries[0] + "," + entries[1] + ",False)")
+  //  }
+
+  //  if (waiterRequests.includes(req.body.table) == false) {
+  //    waiterRequests.push(req.body.table);
+
+  //  }
+  //  res.send("Waiter add request recived")
+})
+
+app.post('/order/requestWaiter/remove', urlencodedParser, function (req, res) {
+  if (!waiterRequests.includes(req.body.table)) {
+    //waiterRequests.splice(waiterRequests.indexOf(req.body.table,1))
+
+  } else {
+
+    waiterRequests.splice(waiterRequests.indexOf(req.body.table), 1)
+  }
+  console.log("Waiter remove request recieved");
+  //console.log(waiterRequests);
+  res.send("Waiter remove request recived")
+})
 
 app.get('/waiter/waiterRequests', function (req, res) {
   //console.log("request recieved");
@@ -92,7 +134,7 @@ function selectquery(sql, res, columns) {
 
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
-    
+
     console.log(result[0].foodtest);
     res.send(result)
   });
@@ -102,12 +144,12 @@ function selectquery(sql, res, columns) {
 function deletequery(sql, res, columns) {
 
   con.query(sql, function (err, result, fields) {
-    
+
     // console.log(result[1].foodtest);
   });
 
 };
-  
+
 
 
 
